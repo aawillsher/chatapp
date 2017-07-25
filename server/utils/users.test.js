7b8edaf20c9da6c235da70e4d626d16a28499472
Adrian Willsher <aawillsher@hotmail.com>
2017-07-24 23:20:37 -0400

    Add Users class

---
+++ server/utils/users.test.js
@@ -0,0 +1,76 @@
+const expect = require('expect');
+
+const {Users} = require('./users');
+
+describe('Users', () => {
+
+  var users;
+
+  beforeEach(() => {
+    users = new Users();
+    users.users = [{
+      id: '1',
+      name: 'Mike',
+      room: 'Node Course'
+    },
+    {
+      id: '2',
+      name: 'Nancy',
+      room: 'jQuery Course'
+    },
+    {
+      id: '3',
+      name: 'Oscar',
+      room: 'Node Course'
+    }]
+  });
+
+  it('should add new user', () => {
+    var users = new Users();
+    var user = {
+      id: '123',
+      name: 'Andrew',
+      room: 'The Office Fans'
+    };
+    var resUser = users.addUser(user.id, user.name, user.room);
+    expect(users.users).toEqual([user]);
+  });
+
+  it('should remove a user', () => {
+      var userId = '1';
+      var user = users.removeUser(userId);
+      expect(user.id).toBe(userId);
+      expect(users.users.length).toBe(2);
+  });
+
+  it('should not remove a user', () => {
+    var userId = '99';
+    var user = users.removeUser(userId);
+    expect(user).toNotExist;
+    expect(users.users.length).toBe(3);
+  });
+
+  it('should find user', () => {
+    var userId = '2';
+    var user = users.getUser(userId);
+    expect(user.id).toBe(userId);
+  });
+
+  it('should not find a user', () => {
+    var userId = '99';
+    var user = users.getUser(userId);
+    expect(user).toNotExist();
+
+  });
+
+  it('should return names for node course', () => {
+    var userList = users.getUserList('Node Course');
+    expect(userList).toEqual(['Mike', 'Oscar']);
+  });
+
+  it('should return names for jQuery course', () => {
+    var userList = users.getUserList('jQuery Course');
+    expect(userList).toEqual(['Nancy']);
+  });
+
+});
